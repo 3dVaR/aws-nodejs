afc31f1fb2e2ad615eda58f2efcd02299ebc9bc8
3dVaR <3dVaR>
2020-11-24 22:10:06 -0500

    adding a mongo data fecth

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-var express = require("express"),
-    app = express(),
-    bodyParser  = require("body-parser"),
-    methodOverride = require("method-override");
-    mongoose = require('mongoose');
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(methodOverride());
-
-var router = express.Router();
-
-router.get('/', function(req, res) {
-   res.send("Hello World!");
-});
-
-app.use(router);
-
-app.listen(3000, function() {
-  console.log("Node server running on http://localhost:3000"");
-});
+// Utilizar funcionalidades del Ecmascript 6
+'use strict'
+// Cargamos el módulo de mongoose para poder conectarnos a MongoDB
+var mongoose = require('mongoose');
+// *Cargamos el fichero app.js con la configuración de Express
+var app = require('./app');
+// Creamos la variable PORT para indicar el puerto en el que va a funcionar el servidor
+var port = 3800;
+// Le indicamos a Mongoose que haremos la conexión con Promesas
+mongoose.Promise = global.Promise;
+// Usamos el método connect para conectarnos a nuestra base de datos
+mongoose.connect('mongodb://localhost:27017/admin',  { useMongoClient: true})
+    .then(() => {
+        // Cuando se realiza la conexión, lanzamos este mensaje por consola
+        console.log("La conexión a la base de datos curso_mean_social se ha realizado correctamente")
+    
+        // CREAR EL SERVIDOR WEB CON NODEJS
+        app.listen(port, () => {
+            console.log("servidor corriendo en http://localhost:3800");
+        });
+    })
+    // Si no se conecta correctamente escupimos el error
+    .catch(err => console.log(err));
